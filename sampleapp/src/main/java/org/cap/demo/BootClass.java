1d9bc311bce556d6eb6e37a4747b1ded68a96bcb
Hemal2711 <hemallodaya27@gmail.com>
2021-09-22 16:01:44 +0530

    sum added in show method

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -7,6 +7,13 @@ public class BootClass {
        show();
 	}
   public static void show() {
+	  int[] arr= {1,2,3,4,4};
+	  int sum=0;
+	  for(int n:arr)
+	  {
+		  sum+=n;
+		  System.out.println(n);
+	  }
 	  System.out.println("Show Method here!");
   }
 }
